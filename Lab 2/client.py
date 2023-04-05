f45d0fd4c7f1cc38841ebd714a83163d543ef347
Vittorio Sironi <98646453+vittoriosironi@users.noreply.github.com>
2023-04-05 11:17:40 +0200

    First commit

---
+++ Lab 2/client.py
@@ -0,0 +1,25 @@
+# importo la libreria
+from socket import *
+
+# apro socket per comunicare UDP
+clientSocket = socket(AF_INET, SOCK_DGRAM) # AF_INET -> IPv4, SOCK_DGRAM -> protocollo UDP
+
+
+# chiedi una stringa all'utente
+message = input("Inserisci una stringa: ") # l'utente inserisce le stringhe
+
+serverPort = 12000 # attraverso la porta 120000, porta usata dal server. La porta usata del server dev'essere aperta
+serverAdress = ("127.0.0.1", serverPort)
+
+# richiedi al server inviando la stringa
+clientSocket.sendto(message.encode("utf-8"), serverAdress) # codifichiamo la sctringa in utf-8 transformadola in binario e mandando i byte tramite la nostra socket UDP
+
+
+# ascolto dalla socket per la risposta
+modifiedMessage, serverAdress = clientSocket.recvfrom(2048) # ricevo il messaggio di conferma dal server con 2048 che è la dimensione massima in byte del messaggio
+
+print("Il server risponde: ", modifiedMessage.decode("utf-8"))
+
+# chiudere la socket
+clientSocket.close()
+print("Il programma è terminato con successo")
---
+++ Lab 2/server.py
@@ -0,0 +1,9 @@
+from socket import *
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+# ciclo infinito
+
+# modifico il messaggio
+
+# rispondo tramite la socket
