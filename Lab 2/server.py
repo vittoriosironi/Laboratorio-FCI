498fc4c84928ee088e5b1760d019ef07b9b42d55
Vittorio Sironi <98646453+vittoriosironi@users.noreply.github.com>
2023-04-05 11:26:43 +0200

    First commit

--- Lab 2/server.py
+++ Lab 2/server.py
@@ -1,9 +1,13 @@
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(("", serverPort)) # lasciamo una stringa vuota perchè il pycharm andrà a riempirlo lui e server port che indica il numero di porta della nostra socket
+# La parentesi interna è per definire una tuple
+# bind = lega
 
-# ciclo infinito
-
-# modifico il messaggio
+while 1:
+    message, clientAddr = serverSocket.recvfrom(2048) # 2048 -> dimensione massima del buffer. La funzione ritorna due parametri ovvero il messaggio inviato e l'indirizzo di chi ci sta contattando
+    modifiedMessage = message.decode("utf-8")
+    modifiedMessage = modifiedMessage.upper()
 
 # rispondo tramite la socket
